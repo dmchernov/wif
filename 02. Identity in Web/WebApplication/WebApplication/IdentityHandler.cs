6c0f1ed57d020abb21b72c891ee95a483bd6de1a
Dmitrii Chernov <dm.chernov@outlook.com>
2017-12-24 15:22:16 +0400

    Task2

--- 02. Identity in Web/WebApplication/WebApplication/IdentityHandler.cs
+++ 02. Identity in Web/WebApplication/WebApplication/IdentityHandler.cs
@@ -1,4 +1,5 @@
-﻿using System.Web;
+﻿using System.Security.Claims;
+using System.Web;
 
 namespace WebApplication
 {
@@ -13,10 +14,29 @@ namespace WebApplication
 			context.Response.Write($"Is authenticated: {user.Identity.IsAuthenticated}");
 			context.Response.Write("</br>");
 			context.Response.Write($"User: {user.Identity.Name}");
+
+			var cIdentity = context.User.Identity as ClaimsIdentity;
+			if (cIdentity != null)
+			{
+				context.Response.Write("<br/>");
+				foreach (var c in cIdentity.Claims)
+				{
+					PrintClaim(context, c);
+				}
+			}
 		}
 
 		/// <summary>Gets a value indicating whether another request can use the <see cref="T:System.Web.IHttpHandler" /> instance.</summary>
 		/// <returns>true if the <see cref="T:System.Web.IHttpHandler" /> instance is reusable; otherwise, false.</returns>
 		public bool IsReusable => true;
+
+		private void PrintClaim(HttpContext context, Claim claim)
+		{
+			context.Response.Write("<hr/>");
+			context.Response.Write($"Issuer : {claim.Issuer}<br/>");
+			context.Response.Write($"Type : {claim.Type}<br/>");
+			context.Response.Write($"Value : {claim.Value}<br/>");
+			//context.Response.Write("<br/>");
+		}
 	}
 }
